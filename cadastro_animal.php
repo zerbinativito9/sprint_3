0ee01f7037c27faeafc1336cad266c48933a0097
zerbinati <aquelazerbinati@gmail.com>
2024-12-03 13:39:07 -0300

    commit

--- cadastro_animal.php
+++ cadastro_animal.php
@@ -49,7 +49,7 @@ $Clientes = $conn->query("SELECT CodCliente, Cliente FROM Clientes");
 <head>
     <meta charset="UTF-8">
     <title>Cadastro de Produto</title>
-    <link rel="stylesheet" href="css/animal.css">
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <div class="container">
